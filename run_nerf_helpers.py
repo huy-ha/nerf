aa4e0cc5b7714267b7351d08ed439fcf4acf6706
iamsusiep <sujip@google.com>
2020-12-09 23:12:59 -0500

    debug lines

--- run_nerf_helpers.py
+++ run_nerf_helpers.py
@@ -859,6 +859,7 @@ def create_nerf(args):
     else:
         ckpts = [os.path.join(basedir, expname, f) for f in sorted(os.listdir(os.path.join(basedir, expname))) if
                  ('model_' in f and 'fine' not in f and 'optimizer' not in f)]
+    ckpts = ['/home/sujipark/nerf/debug/model_000050.npy'] # TODO hardcode
     print('Found ckpts')
     pprint(ckpts)
     if len(ckpts) > 0 and not args.no_reload:
